6436b3380e4aa7185cb21a9057c6e9655f50e600
dauid64 <carlosdauid64br@gmail.com>
2025-07-29 21:41:39 -0300

    adicionando retorno para página principal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,13 +13,15 @@ export default function Header() {
     <header className="bg-white">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
-          <span className="sr-only">LAMFO</span>
-          <Image
-            src="/logo.png"
-            alt="Logo"
-            width={100}
-            height={40}
-          />
+          <Link href="/">
+            <span className="sr-only">LAMFO</span>
+            <Image
+              src="/logo.png"
+              alt="Logo"
+              width={100}
+              height={40}
+            />
+          </Link>
         </div>
         <div className="flex lg:hidden">
           <button
